f1f884188d4a4e5234a638a5fad1e3a16fa5cf5e
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-01 10:30:03 +0200

    created temporary task component

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,5 +1,12 @@
 function Task() {
 
+    return (
+        <li className='task'>
+            <p className="task-desc">Task descritption</p>
+            <span className="task-date">{new Date().toLocaleString()}</span>
+        </li>
+    )
+
 }
 
 export default Task
