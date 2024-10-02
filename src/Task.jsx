4fde692d025a4e0238c6c3363a19197bf36aff28
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-02 10:56:58 +0200

    task component css

--- src/Task.jsx
+++ src/Task.jsx
@@ -9,13 +9,16 @@ function Task(props) {
         <li className='task' >
             <p className="task-desc">{taskDesc}</p>
             <span className="task-date">{new Date().toLocaleString()}</span>
+            <button className="task-button task-button--done">✔ Done</button>
+            <button className="task-button task-button--edit">✎ Edit</button>
+            <button className="task-button task-button--delete">✘ Delete</button>
         </li>
     )
 
 }
 
 Task.propTypes = {
-    taskDesc: PropTypes.string,
+    taskDesc: PropTypes.string.isRequired,
 }
 
 export default Task
