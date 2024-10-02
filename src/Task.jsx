e7200582906176b7a3959b334eddddba86259322
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-02 11:04:17 +0200

    task component css

--- src/Task.jsx
+++ src/Task.jsx
@@ -7,11 +7,18 @@ function Task(props) {
 
     return (
         <li className='task' >
+
+            <div className='task-directional-controls'>
+                <button className="task-button">▲</button>
+                <button className="task-button">▼</button>
+            </div>
+
             <p className="task-desc">{taskDesc}</p>
             <span className="task-date">{new Date().toLocaleString()}</span>
             <button className="task-button task-button--done">✔ Done</button>
             <button className="task-button task-button--edit">✎ Edit</button>
-            <button className="task-button task-button--delete">✘ Delete</button>
+            <button className="task-button task-button--delete">X Delete</button>
+           
         </li>
     )
 
