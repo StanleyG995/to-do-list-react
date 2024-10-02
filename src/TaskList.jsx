5e027125c6425f668d35209c619a587c0dbbf1a1
Stanislaw <goral.stanislaw95@gmail.com>
2024-10-02 19:39:15 +0200

    added dummy tasks

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -10,6 +10,13 @@ function TaskList() {
             <Task taskDesc="Task 1" />
             <Task taskDesc="Task 2" />
             <Task taskDesc="Task 3" />
+            <Task taskDesc="Task 4" />
+            <Task taskDesc="Task 5" />
+            <Task taskDesc="Task 6" />
+            <Task taskDesc="Task 7" />
+            <Task taskDesc="Task 8" />
+            <Task taskDesc="Task 9" />
+            <Task taskDesc="Task 10" />
        </ul> 
     )
     
