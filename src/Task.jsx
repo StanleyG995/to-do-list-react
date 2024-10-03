2a1c78af62918ffa8c35084a8af6d7bb7a1d188a
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-03 14:28:42 +0200

    fixed code

--- src/Task.jsx
+++ src/Task.jsx
@@ -3,8 +3,6 @@ import './Task.css'
 
 function Task(props) {
 
-    const taskDesc = props.taskDesc
-
     return (
         <li className='task' >
 
@@ -13,19 +11,21 @@ function Task(props) {
                 <button className="task-button">▼</button>
             </div>
 
-            <p className="task-desc">{taskDesc}</p>
+            <p className="task-desc">{props.taskDesc}</p>
             <span className="task-date">{new Date().toLocaleString()}</span>
             <button className="task-button task-button--done">✔ Done</button>
             <button className="task-button task-button--edit">✎ Edit</button>
             <button className="task-button task-button--delete">X Delete</button>
            
         </li>
+
+        
     )
 
 }
 
 Task.propTypes = {
-    taskDesc: PropTypes.string.isRequired,
+    taskDesc: PropTypes.string,
 }
 
 export default Task
