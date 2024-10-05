1327f68f0b6106f527a1d84e1be414480a3dbcf2
Stanislaw <goral.stanislaw95@gmail.com>
2024-10-05 09:18:45 +0200

    styled app title

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
 
   return (
     <div className='app-wrapper'>
-      <h1 className='app-title'>To do list</h1>
+      <h1 className='app-title'><span style={{color: 'rgb(6, 30, 216)',}}>☑</span> To do list</h1>
       <AddNewTask>
         <InputModal />
       </AddNewTask>
 
-      <h2 className='app-title'>Current tasks</h2>
+      <h2 className='list-title'>Current tasks:</h2>
       <TaskList />  
     
     </div>
