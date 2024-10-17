8060aef388665c008b9dd045383d1d18d78e7223
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-17 13:18:32 +0200

    fix: "class" to "className" in menu-items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
             <div className="row">
                 <div className="column">
                     <ul className="menu">
-                        <a href="" className="menu-item menu-item--active text-medium"><i class="fa-solid fa-list-check menu-item-icon"></i> Current</a>
-                        <a href="" className="menu-item text-medium"><i class="fa-solid fa-clock-rotate-left menu-item-icon"></i> History</a>
-                        <a href="" className="menu-item text-medium"><i class="fa-solid fa-trash menu-item-icon"></i> Deleted</a>
+                        <a href="" className="menu-item menu-item--active text-medium"><i className="fa-solid fa-list-check menu-item-icon"></i> Current</a>
+                        <a href="" className="menu-item text-medium"><i className="fa-solid fa-clock-rotate-left menu-item-icon"></i> History</a>
+                        <a href="" className="menu-item text-medium"><i className="fa-solid fa-trash menu-item-icon"></i> Deleted</a>
                     </ul>
                 </div>
             </div>
