9819d5620cfee60e0d32b2c25bb37648c4d02aa8
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-18 14:23:55 +0200

    feat: created modal component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import ErrorMessage from './components/UI/ErrorMessage/ErrorMessage.jsx'
 
 
 const App = () => {
-	
 	return (
 		<TaskProvider>
 			<div className='app-wrapper'>
---
+++ src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import './Modal.css'
+
+import Button from '../Button/Button.jsx'
+
+const Modal = ( {children, buttonPrimary} ) => {
+    return (
+        <div className="modal-overlay">
+            <div className="modal">
+                <div className="row row--flex-center modal-content ">
+                    { children }
+                </div>
+                <div className="row row--flex-center modal-buttons ">
+                    <Button children={'Cancel'} classes='button button--m button--secondary' onClick={() => {console.log("Modal closed")}}/>
+                    { buttonPrimary }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+Modal.propTypes = {
+    children: PropTypes.node,
+    buttonPrimary: PropTypes.node,
+}
+
+
+export default Modal
