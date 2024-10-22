70dc98a38d921353d7ad62a08b7a897ac4175d48
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-22 10:26:03 +0200

    fix: fixed class to className in FA icons

--- src/components/UI/List/List.jsx
+++ src/components/UI/List/List.jsx
@@ -13,11 +13,11 @@ const List = () => {
     return (
         <div className='list'>
              <div className="list-header">
-                <button className='list-heading category-heading'>Category <i class="fa-solid fa-caret-up list-heading-caret"></i></button>
-                <button className='list-heading description-heading'>Description <i class="fa-solid fa-caret-up list-heading-caret"></i></button>
-                <button className='list-heading date-added-heading'> Added <i class="fa-solid fa-caret-up list-heading-caret"></i></button>
-                <button className='list-heading date-due-heading'>Due <i class="fa-solid fa-caret-up list-heading-caret"></i></button>
-                <button className='list-heading priority-heading'>Priority <i class="fa-solid fa-caret-up list-heading-caret"></i></button>
+                <button className='list-heading category-heading'>Category <i className="fa-solid fa-caret-up list-heading-caret"></i></button>
+                <button className='list-heading description-heading'>Description <i className="fa-solid fa-caret-up list-heading-caret"></i></button>
+                <button className='list-heading date-added-heading'> Added <i className="fa-solid fa-caret-up list-heading-caret"></i></button>
+                <button className='list-heading date-due-heading'>Due <i className="fa-solid fa-caret-up list-heading-caret"></i></button>
+                <button className='list-heading priority-heading'>Priority <i className="fa-solid fa-caret-up list-heading-caret"></i></button>
             </div>
             <ul className='list-inner'>
                 {
