b8066b836405e051f1b87bc499b5b6f10c484e2e
Stanlej95 <goral.stanislaw95@gmail.com>
2024-10-22 08:42:30 +0200

    feat: added list header

--- src/components/UI/List/List.jsx
+++ src/components/UI/List/List.jsx
@@ -11,13 +11,20 @@ const List = () => {
     const { currentTasks, setCurrentTasks } = useContext( TaskContext )
 
     return (
-     
+        <>
+             <div className="list-header">
+                <button>Category</button>
+                <button>Description</button>
+                <button>Added</button>
+                <button>Due</button>
+                <button>Priority</button>
+            </div>
             <ul className='list'>
                 {
                     listDisplay( currentTasks )
                 }
             </ul>
-  
+        </>
         
     )    
 }
--- src/components/UI/ListItem/ListItem.jsx
+++ src/components/UI/ListItem/ListItem.jsx
@@ -5,17 +5,19 @@ import './ListItem.css'
 const ListItem = ({ id, category, categoryIcon, description, dateAdded, dateDue, priority }) => {
 
     return (
-        <li className='list-item' key={id} id={id}>
-            <p className="list-item-property category"><i className={`category-icon ${categoryIcon}`}></i> {category}</p>
-            <p className="list-item-property description">{description}</p>
-            <p className="list-item-property dateAdded">
-            {dateAdded}
-            </p>
-            <p className="list-item-property dateDue">{dateDue}</p>
-            <p className={`list-item-property priority priority--${priority}`}>
-            {priority}
-            </p>
-        </li>
+        <>
+            <li className='list-item' key={id} id={id}>
+                <p className="list-item-property category"><i className={`category-icon ${categoryIcon}`}></i> {category}</p>
+                <p className="list-item-property description">{description}</p>
+                <p className="list-item-property dateAdded">
+                {dateAdded}
+                </p>
+                <p className="list-item-property dateDue">{dateDue}</p>
+                <p className={`list-item-property priority priority--${priority}`}>
+                {priority}
+                </p>
+            </li>
+        </>
     )
 }
 
