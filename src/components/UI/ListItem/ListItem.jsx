2af4c0873657cc803d2952fde41e1a1b9f1a7a81
StanislawG <goral.stanislaw95@gmail.com>
2024-11-03 18:16:43 +0100

    style: trash bin tasks

--- src/components/UI/ListItem/ListItem.jsx
+++ src/components/UI/ListItem/ListItem.jsx
@@ -25,7 +25,7 @@ const ListItem = ({ id, category, categoryIcon, description, dateAdded, dateDue,
                 <p className={`list-item-property priority priority--${priority}`}>
                 {priority}
                 </p>
-                <div className='list-item-property row'>
+                <div className='list-item-property list-buttons row'>
                     <Button classNames='button button--s button--primary list-item-property' onClick={(e) => { getTaskId(e); handleModalOpen('completeTask')}}><i className="fa-solid fa-check"></i> Complete</Button>
                     <Button classNames='button button--s button--secondary list-item-property' onClick={(e) => { getTaskId(e); handleModalOpen('editTask')}}><i className="fa-solid fa-pencil"></i> Edit</Button>
                     <Button classNames='button button--s button--danger list-item-property' onClick={(e) => {getTaskId(e); handleModalOpen('deleteTask')}}><i className="fa-solid fa-xmark"></i> Delete</Button>
